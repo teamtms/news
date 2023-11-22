797797a858f5563cf823355f1ed0c65994c67d74
FB24M <103760918+iFB24M@users.noreply.github.com>
2023-11-23 00:19:36 +0400

    Add files via upload

---
+++ assets/index-1ea45257.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-235cf4d9.js";const s="_title2_1jkhc_1",l={title2:s},c=t=>e.jsx("h2",{...t,className:`${t.className} ${l.title2}`,children:t.children});export{c as default};
